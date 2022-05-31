701b872fcbfd1eb82a062693740859af97b12c25
ibrahimsarikaya <ibo7534@gmail.com>
2022-05-31 18:46:20 +0300

    Codding

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace array_sinifi_methodlar_ // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
